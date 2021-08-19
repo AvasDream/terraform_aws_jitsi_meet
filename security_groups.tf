ad55d3919ec4913a16c45bf612dba1bab0fafc6f
George Ziady <george@springfisher.com>
2021-08-19 19:55:02 +0200

    Extend open UDP port range.

--- security_groups.tf
+++ security_groups.tf
@@ -39,8 +39,8 @@ resource "aws_security_group" "allow_connections_jitsi-meet" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 53
-    to_port     = 53
+    from_port   = 0
+    to_port     = 65535
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
